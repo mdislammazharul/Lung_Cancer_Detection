f84e6de5dab8242431feda221d99c2f2b78be0f9
mdislammazharul <mazharul.py@gmail.com>
2025-12-30 12:59:22 +0100

    Fix vite config: import defineConfig

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
-    plugins: [react()],
-    base: "/Lung_Cancer_Detection/",
-  });
+  plugins: [react()],
+  base: "/Lung_Cancer_Detection/",
+});
